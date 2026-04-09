10ccb7ea135e70466e13178f2296c9085faee353
Harshit Jain <harshitostwal1234@gmail.com>
2026-04-09 20:32:41 +0530

    feat: add scroll-to-top functionality on route change in MainLayout

--- src/layout/main-layout.jsx
+++ src/layout/main-layout.jsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Outlet } from "react-router";
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import Footer from "@/components/common/footer";
 import Navbar from "@/components/common/navbar";
 
 function MainLayout() {
-    return (
-        <div className="mx-auto flex w-11/12 flex-col gap-10 py-5 2xl:max-w-4/5">
-            <Navbar />
-            <div className="flex-1">
-                <Outlet />
-            </div>
-            <Footer />
-        </div>
-    );
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "instant" });
+  }, [location.pathname]);
+
+  return (
+    <div className="mx-auto flex w-11/12 flex-col gap-10 py-5 2xl:max-w-4/5">
+      <Navbar />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default MainLayout;
