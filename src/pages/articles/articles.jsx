2df9a3a08ed97cbc3e3a54dad5b651d4a5d86fae
Harshit Jain <harshitostwal1234@gmail.com>
2026-04-13 23:22:49 +0530

    refactor: improve code formatting and structure in Articles component

--- src/pages/articles/articles.jsx
+++ src/pages/articles/articles.jsx
@@ -1,21 +1,31 @@
 import React, { useMemo } from "react";
 import ArticleCard from "@/components/common/article-card";
 import { useArticle } from "@/providers/article-provider";
+import { Heading } from "@/components/ui/Headings";
 
 function Articles() {
-    const { fetchArticles } = useArticle();
+  const { fetchArticles } = useArticle();
 
-    const articlesData = useMemo(() => fetchArticles(0), [fetchArticles]);
+  const articlesData = useMemo(() => fetchArticles(0), [fetchArticles]);
 
-    return (
-        <div className="flex flex-col gap-20">
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
-                {articlesData.map((article, idx) => (
-                    <ArticleCard key={idx} article={article} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-20">
+      <div className="flex flex-col gap-1">
+        <Heading size="h3" className="font-semibold">
+          All Articles
+        </Heading>
+        <Heading size="h6" className="text-muted-foreground">
+          Explore the latest articles on various topics, written by our talented
+          authors.
+        </Heading>
+      </div>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
+        {articlesData.map((article, idx) => (
+          <ArticleCard key={idx} article={article} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Articles;
